963f9df71ce477af11ae3ed4c34eb8e391a2206c
dinhnguyencntt <dn1994099@gmail.com>
2024-03-11 09:23:03 +0700

    Add project files.

---
+++ LTCB_forlong2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so; cout << "nhap so:"; cin >> so;
+	for (int i = 0; i < so; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < so; i++) {
+		for (int j = so; j > i; j--) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
